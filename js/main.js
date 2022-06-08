63dc48a3ffd1371a3b5ca25a04d8126bc0497c78
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2022-06-08 12:32:31 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 console.log("main.js!!");
 
+// ServiceWorker(If you needed...)
+navigator.serviceWorker.register("./pwa/service_worker.js");
+
 const SS_GOOGLE = "https://docs.google.com/spreadsheets/d/";
 const SS_ID     = "1CzccPnNKqIEgTPyvq83w28H0t5ycmuhe_EBarAdy4xY";
 const SS_CSV    = "/export?format=csv";
