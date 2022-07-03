d810a90bcbead35f824771d7cdb8e20413840591
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2022-07-03 12:10:34 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ console.log("main.js!!");
 
 //==========
 // ScreenOrientation
-const orientation = screen.msOrientation || screen.mozOrientation || (screen.orientation || {});
-screen.orientation.lock("portrait").catch((err)=>{console.log(err);});
+// const orientation = screen.msOrientation || screen.mozOrientation || (screen.orientation || {});
+// screen.orientation.lock("portrait").catch((err)=>{console.log(err);});
 
-try{
-	window.screen.lockOrientation("portrait");
-}catch(err){
-	console.log(err);
-}
+// try{
+// 	window.screen.lockOrientation("portrait");
+// }catch(err){
+// 	console.log(err);
+// }
 
 //==========
 // ServiceWorker(If you needed...)
